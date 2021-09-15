c0f9487ac49dd7f488f3a3cb3d6e38c70478dfea
Bob <54867422+Bob-by@users.noreply.github.com>
2021-09-15 20:09:55 +0200

    Type-A-Name fix

--- src/namepanel/NameCheck.py
+++ src/namepanel/NameCheck.py
@@ -97,7 +97,7 @@ def checkName(name, otherCheckFuncs=[], font=None):
             tn = TextNode('NameCheck')
             tn.setFont(font)
             for c in name:
-                if not tn.hasCharacter(ord(c)):
+                if not tn.hasCharacter(str(c)):
                     notify.info('name contains bad char: %s' % TextEncoder().encodeWtext(c))
                     return OTPLocalizer.NCBadCharacter % TextEncoder().encodeWtext(c)
 
